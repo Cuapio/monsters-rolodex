b9a147501f478c60dc2ec0e2626e253d995df5e4
Mario Cuapio <cuapioman@gmail.com>
2019-10-24 18:12:22 -0500

    Updates

---
+++ precache-manifest.50eb4ff522666180012950376666c4a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e7fe19c6d37bc54300cbf85ba54efc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac3d8b035bb970743156",
+    "url": "/monsters-rolodex/static/css/main.73e4f700.chunk.css"
+  },
+  {
+    "revision": "18c732575b1b647cb901",
+    "url": "/monsters-rolodex/static/js/2.fb9ce0bc.chunk.js"
+  },
+  {
+    "revision": "ac3d8b035bb970743156",
+    "url": "/monsters-rolodex/static/js/main.59aa7442.chunk.js"
+  },
+  {
+    "revision": "d9b8c0eaa5248eff7b16",
+    "url": "/monsters-rolodex/static/js/runtime-main.34ddeafe.js"
+  }
+]);
